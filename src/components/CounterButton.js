fdc31092cd6c9204651f19de8fd6fd4739e8c83d
Jonathan Schwartz <filmboy3@yahoo.com>
2019-01-30 20:59:08 -0500

    added shouldComponentUpdate CounterButton

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 class CounterButton extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0
+        }
+    }
     shouldComponentUpdate(nextProps, nextState) {
-        return false;
+        if (this.state.count !== nextState.count) {
+            return true
+        }
+        return false
+    }
+
+    updateCount = () => {
+        this.setState(state => {
+            return { count: state.count + 1 }
+        })
     }
 
     render() {
         console.log('CounterButton')
-        return <h1 className='f1'>RoboFriends</h1>
+        return (
+            <button color={this.props.color} onClick={this.updateCount}>
+                Count: {this.state.count}
+            </button>
+        );
     }
 }
 
