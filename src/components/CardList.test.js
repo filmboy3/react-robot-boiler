ec62f21fd8013e7c926145b327525f3170fc1ad4
Jonathan Schwartz <filmboy3@yahoo.com>
2019-01-31 19:45:11 -0500

    added cardList shallow test

---
+++ src/components/CardList.test.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import CardList from './CardList';
+
+it('expect to render CardList component', () => {
+    const mockRobots = [
+        {
+            id: 1,
+            name: 'Jon Snow',
+            username: 'JonJon',
+            email: 'jony@gmail.com'
+        }
+    ]
+    expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
+})
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const SearchBox = ({ searchField, searchChange }) => {
-  console.log("Rendered SearchBox");
   return (
     <div className='tc pa2'>
       <input
